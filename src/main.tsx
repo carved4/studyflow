5525b3dd271e6914172aa1c29d96eadf3a486d2f
carved4 <osmith26@adrian.edu>
2024-12-03 13:30:50 -0500

    Fix Vercel deployment: Replace type assertion with null check in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,11 @@ import App from './App'
 import './index.css'
 import './styles/global.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
